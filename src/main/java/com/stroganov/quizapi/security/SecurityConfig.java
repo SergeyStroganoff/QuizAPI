7745c0fc620a4bd7ab8fcbc99794c141d5656f73
Sergey Stroganov <111sergey90@gmail.com>
2022-04-17 01:16:17 +0300

    Audience validator created. Added spring security dependencies

--- src/main/java/com/stroganov/quizapi/security/SecurityConfig.java
+++ src/main/java/com/stroganov/quizapi/security/SecurityConfig.java
@@ -5,18 +5,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//@Configuration
 @EnableWebSecurity
+//@Profile(value = {"development", "production"})
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers(HttpMethod.GET, "/api/quiz/**").permitAll() // GET requests don't need auth
+                .mvcMatchers(HttpMethod.GET, "/api/quiz/**").permitAll()// GET requests don't need auth
                 .anyRequest()
                 .authenticated()
                 .and()
                 .oauth2ResourceServer()
                 .jwt();
     }
-
 }
